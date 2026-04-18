06c247cba84a161c1d8387b269fda925c17236cc
Rafael <rafael.quinterosv@gmail.com>
2026-04-18 21:52:24 +0200

    Fix auth config

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, getRedirectResult } from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCYWUXSvr8SX-FdcWvjD6RH7bP02817Okk",
@@ -13,12 +13,17 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+export const db = getFirestore(app, 'trips')
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
 googleProvider.setCustomParameters({
   prompt: 'select_account'
 })
 
-export const loginWithGoogle = () => signInWithPopup(auth, googleProvider)
+export const loginWithGoogle = async () => {
+  const result = await signInWithPopup(auth, googleProvider)
+  return result
+}
+
+export const checkRedirectResult = () => getRedirectResult(auth)
 export const logout = () => signOut(auth)
